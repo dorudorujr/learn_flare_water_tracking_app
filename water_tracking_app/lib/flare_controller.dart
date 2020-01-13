8681753eab0de8c9765cf8a21a3b2661d6e26eae
seiyasugioka <sugiokaseiya@team-lab.com>
2020-01-13 16:45:13 +0900

    コメント修正

--- water_tracking_app/lib/flare_controller.dart
+++ water_tracking_app/lib/flare_controller.dart
@@ -12,9 +12,11 @@ class AnimationControls extends FlareController {
   FlutterActorArtboard _artboard;
 
   ///塗りつぶしアニメーション。水摂取量を追加/削減するたびにこれをアニメーション化できます
+  ///水が増えるアニメーション
   ActorAnimation _fillAnimation;
 
   ///現在の取水量に基づいてY軸上を移動する氷
+  ///iceboyの上がるアニメーション
   ActorAnimation _iceboyMoveY;
 
   ///アニメーションのミックスに使用
@@ -29,25 +31,34 @@ class AnimationControls extends FlareController {
   double _smoothTime = 5;
 
   void initialize(FlutterActorArtboard artboard) {
-    ///ここでアニメーションとアートボードのリファレンスを取得します
+    ///ここでアニメーションとアートボードの参照を取得
     _artboard = artboard;
     _fillAnimation = artboard.getAnimation("water up");
     _iceboyMoveY = artboard.getAnimation("iceboy_move_up");
   }
 
+  ///フレームごとに呼び出される
+  ///アニメーションの現在のビューマトリックスに関する情報をリレーします。
   void setViewTransform(Mat2D viewTransform) {}
 
+  ///フレームごとに呼び出されます
+  ///経過時間をコントローラにリレーします。これにより、カスタムアクションを実行できます。
   ///Artboard:flareの基盤のパーツ
   bool advance(FlutterActorArtboard artboard, double elapsed) {
 
-    ///artboardが"Artboardなら"
+    ///artboardが"Artboard"なら
     if(artboard.name.compareTo("Artboard") == 0) {
+
+      ///現在の水の量の計算
       _currentWaterFill += (_waterFill-_currentWaterFill) * min(1, elapsed *
           _smoothTime);
+
+      ///アニメーションを再生するとき、アニメーションオブジェクトでapply（）メソッドを呼び出す必要がある
       _fillAnimation.apply( _currentWaterFill * _fillAnimation.duration, artboard, 1);  ///任意の時間アニメーションを制御
       _iceboyMoveY.apply(_currentWaterFill * _iceboyMoveY.duration, artboard, 1);       ///任意の時間(_currentWaterFill * _iceboyMoveY.duration)アニメーションを制御
     }
 
+    ///_baseAnimationsのindexを取得
     int len = _baseAnimations.length - 1;
 
     for (int i = len; i >= 0; i--) {
